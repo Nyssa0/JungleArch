5e4fb19d7ce59e201837cc105ef4b73be4959283
Tekrodz <kevinkmmichel@gmail.com>
2022-09-23 11:14:57 +0200

    Connect horaire in contact with openings in back

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\OpeningRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    /**
+     * @Route("/contact", name="app_contact")
+     */
+    public function index(OpeningRepository $openingRepository): Response
     {
+        $openings = $openingRepository->find(1);
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'openings' => $openings,
         ]);
     }
 }
